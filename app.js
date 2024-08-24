de0b6b56da91c752e79252457b02ace6ec20702f
Aram47 <aram.minasyan.00@gmail.com>
2024-08-24 13:25:14 +0400

    finished

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/test', (req, res) => {
+	console.log("get request url -> /test");
+	res.status(200).send("Yuhouuuu res from get req url -> /test");
+});
+
+app.post('/test', (req, res) => {
+	console.log("post request url -> /test");
+	console.log(req.header);
+	console.log(req.body);
+	res.status(200).send("Tash tash tash res from post req url -> /test");
+});
+
+app.listen(4450);
