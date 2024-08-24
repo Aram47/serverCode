ccd2de67294d73cf3d05602ce5a3af7d657dfdfb
Aram47 <aram.minasyan.00@gmail.com>
2024-08-24 13:36:31 +0400

    some changes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 
-
 app.get('/test', (req, res) => {
 	console.log("get request url -> /test");
 	res.status(200).send("Yuhouuuu res from get req url -> /test");
@@ -9,7 +8,7 @@ app.get('/test', (req, res) => {
 
 app.post('/test', (req, res) => {
 	console.log("post request url -> /test");
-	console.log(req.header);
+	console.log(req.headers);
 	console.log(req.body);
 	res.status(200).send("Tash tash tash res from post req url -> /test");
 });
